090e9d9aa7b9f22b98314ee848e2ae3a8fcb9311
jacinth0727 <intern2@lifesaver.care>
2025-02-05 19:31:21 +0800

    quotation

--- smpc_sales_system/Pages/SetupModal.Designer.cs
+++ smpc_sales_system/Pages/SetupModal.Designer.cs
@@ -29,29 +29,23 @@ namespace smpc_sales_app.Pages
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SetupModal));
             this.panel1 = new System.Windows.Forms.Panel();
             this.lbl_setup_title = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.textBox23 = new System.Windows.Forms.TextBox();
-            this.textBox24 = new System.Windows.Forms.TextBox();
-            this.label27 = new System.Windows.Forms.Label();
-            this.label28 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btn_edit = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.btn_save = new System.Windows.Forms.Button();
-            this.btn_add = new System.Windows.Forms.Button();
             this.dgv_application_setup = new System.Windows.Forms.DataGridView();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.code = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bs_quotation_list = new System.Windows.Forms.BindingSource(this.components);
+            this.ds_quotation_list = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.document_no = new System.Data.DataColumn();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataTable2 = new System.Data.DataTable();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel4.SuspendLayout();
-            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_application_setup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bs_quotation_list)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ds_quotation_list)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -66,177 +60,62 @@ namespace smpc_sales_app.Pages
             // lbl_setup_title
             // 
             this.lbl_setup_title.AutoSize = true;
-            this.lbl_setup_title.Location = new System.Drawing.Point(13, 13);
+            this.lbl_setup_title.Location = new System.Drawing.Point(131, 9);
             this.lbl_setup_title.Name = "lbl_setup_title";
-            this.lbl_setup_title.Size = new System.Drawing.Size(35, 13);
+            this.lbl_setup_title.Size = new System.Drawing.Size(134, 13);
             this.lbl_setup_title.TabIndex = 0;
-            this.lbl_setup_title.Text = "label1";
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.panel4);
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 414);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(400, 92);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.textBox23);
-            this.panel4.Controls.Add(this.textBox24);
-            this.panel4.Controls.Add(this.label27);
-            this.panel4.Controls.Add(this.label28);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(0, 37);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(400, 55);
-            this.panel4.TabIndex = 1;
-            // 
-            // textBox23
-            // 
-            this.textBox23.Location = new System.Drawing.Point(143, 27);
-            this.textBox23.Name = "textBox23";
-            this.textBox23.Size = new System.Drawing.Size(125, 20);
-            this.textBox23.TabIndex = 91;
-            // 
-            // textBox24
-            // 
-            this.textBox24.Location = new System.Drawing.Point(143, 6);
-            this.textBox24.Name = "textBox24";
-            this.textBox24.Size = new System.Drawing.Size(125, 20);
-            this.textBox24.TabIndex = 90;
-            // 
-            // label27
-            // 
-            this.label27.AutoSize = true;
-            this.label27.Location = new System.Drawing.Point(48, 30);
-            this.label27.Name = "label27";
-            this.label27.Size = new System.Drawing.Size(89, 13);
-            this.label27.TabIndex = 89;
-            this.label27.Text = "Percent Discount";
-            // 
-            // label28
-            // 
-            this.label28.AutoSize = true;
-            this.label28.Location = new System.Drawing.Point(5, 9);
-            this.label28.Name = "label28";
-            this.label28.Size = new System.Drawing.Size(132, 13);
-            this.label28.TabIndex = 88;
-            this.label28.Text = "Sub Total Before Discount";
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.btn_edit);
-            this.panel3.Controls.Add(this.button3);
-            this.panel3.Controls.Add(this.btn_save);
-            this.panel3.Controls.Add(this.btn_add);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(400, 37);
-            this.panel3.TabIndex = 0;
-            // 
-            // btn_edit
-            // 
-            this.btn_edit.Location = new System.Drawing.Point(82, 8);
-            this.btn_edit.Name = "btn_edit";
-            this.btn_edit.Size = new System.Drawing.Size(73, 22);
-            this.btn_edit.TabIndex = 111;
-            this.btn_edit.Text = "EDIT";
-            this.btn_edit.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.BackColor = System.Drawing.Color.Red;
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(83, 9);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(73, 22);
-            this.button3.TabIndex = 109;
-            this.button3.Text = "CANCEL";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // btn_save
-            // 
-            this.btn_save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_save.BackColor = System.Drawing.Color.LimeGreen;
-            this.btn_save.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.btn_save.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btn_save.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btn_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_save.ForeColor = System.Drawing.Color.Black;
-            this.btn_save.Location = new System.Drawing.Point(12, 9);
-            this.btn_save.Name = "btn_save";
-            this.btn_save.Size = new System.Drawing.Size(73, 22);
-            this.btn_save.TabIndex = 108;
-            this.btn_save.Text = "SAVE";
-            this.btn_save.UseVisualStyleBackColor = false;
-            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
-            // 
-            // btn_add
-            // 
-            this.btn_add.Location = new System.Drawing.Point(12, 8);
-            this.btn_add.Name = "btn_add";
-            this.btn_add.Size = new System.Drawing.Size(73, 22);
-            this.btn_add.TabIndex = 110;
-            this.btn_add.Text = "ADD";
-            this.btn_add.UseVisualStyleBackColor = true;
+            this.lbl_setup_title.Text = "SALES QUOTATION LIST";
             // 
             // dgv_application_setup
             // 
             this.dgv_application_setup.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_application_setup.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.id,
-            this.code,
-            this.name});
             this.dgv_application_setup.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgv_application_setup.Location = new System.Drawing.Point(0, 47);
             this.dgv_application_setup.MultiSelect = false;
             this.dgv_application_setup.Name = "dgv_application_setup";
             this.dgv_application_setup.ReadOnly = true;
-            this.dgv_application_setup.Size = new System.Drawing.Size(400, 367);
+            this.dgv_application_setup.Size = new System.Drawing.Size(400, 459);
             this.dgv_application_setup.TabIndex = 2;
+            this.dgv_application_setup.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_application_setup_CellContentDoubleClick);
             // 
-            // id
+            // bs_quotation_list
             // 
-            this.id.DataPropertyName = "id";
-            this.id.HeaderText = "id";
-            this.id.Name = "id";
-            this.id.ReadOnly = true;
-            this.id.Visible = false;
+            this.bs_quotation_list.DataSource = this.ds_quotation_list;
+            this.bs_quotation_list.Position = 0;
             // 
-            // code
+            // ds_quotation_list
             // 
-            this.code.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.code.DataPropertyName = "code";
-            this.code.HeaderText = "CODE";
-            this.code.Name = "code";
-            this.code.ReadOnly = true;
+            this.ds_quotation_list.DataSetName = "NewDataSet";
+            this.ds_quotation_list.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1,
+            this.dataTable2});
             // 
-            // name
+            // dataTable1
             // 
-            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.name.DataPropertyName = "name";
-            this.name.HeaderText = "NAME";
-            this.name.Name = "name";
-            this.name.ReadOnly = true;
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.document_no,
+            this.dataColumn1});
+            this.dataTable1.TableName = "Quotation List";
+            // 
+            // document_no
+            // 
+            this.document_no.ColumnName = "document_no";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "customer_name";
+            // 
+            // dataTable2
+            // 
+            this.dataTable2.TableName = "Table2";
             // 
             // SetupModal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(400, 506);
             this.Controls.Add(this.dgv_application_setup);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -246,11 +125,11 @@ namespace smpc_sales_app.Pages
             this.Load += new System.EventHandler(this.SetupModal_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
-            this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_application_setup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bs_quotation_list)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ds_quotation_list)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -258,21 +137,13 @@ namespace smpc_sales_app.Pages
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.DataGridView dgv_application_setup;
-        private System.Windows.Forms.TextBox textBox23;
-        private System.Windows.Forms.TextBox textBox24;
-        private System.Windows.Forms.Label label27;
-        private System.Windows.Forms.Label label28;
-        private System.Windows.Forms.Button btn_edit;
-        private System.Windows.Forms.Button btn_add;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button btn_save;
         private System.Windows.Forms.Label lbl_setup_title;
-        private System.Windows.Forms.DataGridViewTextBoxColumn id;
-        private System.Windows.Forms.DataGridViewTextBoxColumn code;
-        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.BindingSource bs_quotation_list;
+        private System.Data.DataSet ds_quotation_list;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn document_no;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataTable dataTable2;
     }
 }
