acefc2dcbf9b55da7030349ddac3cb9a584778e7
jacinth0727 <intern2@lifesaver.care>
2025-02-11 15:58:09 +0800

    ALL CHANGES

--- smpc_sales_system/Models/BPI.cs
+++ smpc_sales_system/Models/BPI.cs
@@ -11,26 +11,80 @@ namespace smpc_sales_system.Models
 
         public int id { get; set; }
 
-        //public string name { get; set; }
+        public int sales_id { get; set; }
+
+        public string name { get; set; }
+
+        public string website { get; set; }
+
+        public string tin { get; set; }
+
+        public string tel_no { get; set; }
+
+        public string industry_ids { get; set; }
+
+        public string industry_names { get; set; }
+
+
     }
 
     class BpiGeneral
     {
 
+        public int id { get; set; }
+
         public int based_id { get; set; }
+        public int social_id { get; set; }
         public string branch_name { get; set; }
+
+
+        public string transaction_type { get; set; }
+
+        public string class_name { get; set; }
+
+        public string branch_tel_no { get; set; }
+        public string branch_website { get; set; }
         public string customer_code { get; set; }
+        public string supplier_code { get; set; }
+
+        public string fax_no { get; set; }
+        public string notes { get; set; }
+        public string entity_ids { get; set; }
+        public string entity_names { get; set; }
+
+        public string branch_industry_names { get; set; }
+        public string branch_industry_ids { get; set; }
+
+
+
     }
 
     class BpiContacts
     {
+
+        public int contacts_id { get; set; }
         public int contacts_based_id { get; set; }
         public string number { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string preferences { get; set; }
+        public int position { get; set; }
+
     }
 
     class BpiAddress
     {
+        public int address_id { get; set; }
         public int address_based_id { get; set; }
         public string location { get; set; }
+    }
+
+    class Bpi_Class
+    {
+        public List<Bpi> bpi { get; set; }
+        public List<BpiGeneral> general { get; set; }
+        public List<BpiContacts> contacts { get; set; }
+        public List<BpiAddress> address { get; set; }
+
     }
 }
